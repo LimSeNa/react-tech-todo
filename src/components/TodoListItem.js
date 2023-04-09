d1d7ba41626f0df67fe45972fdb129c5ba8f7472
Sena Lim <tpsk8322@naver.com>
2023-04-10 05:42:04 +0900

    지우기 기능 구현

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,8 +2,8 @@ import {MdCheckBoxOutlineBlank, MdRemoveCircleOutline, MdCheckBox} from "react-i
 import './TodoListItem.scss';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo }) => {
-    const { text, checked } = todo;
+const TodoListItem = ({ todo, onRemove }) => {
+    const { id, text, checked } = todo;
 
     return (
         <div className="TodoListItem">
@@ -11,7 +11,7 @@ const TodoListItem = ({ todo }) => {
                 {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
                 <div className="text">{text}</div>
             </div>
-            <div className="remove">
+            <div className="remove" onClick={() => onRemove(id)}>
                 <MdRemoveCircleOutline/>
             </div>
         </div>
