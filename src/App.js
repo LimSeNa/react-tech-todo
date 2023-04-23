cca2946586112d8453bc1d6c917ba310be5f238f
Sena Lim <tpsk8322@naver.com>
2023-04-23 15:50:04 +0900

    App.js 오타 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {useCallback, useReducer, useRef} from "react";
 
 function createBulkTodos() {
     const array = [];
-    for(let i = 1; i < 2500; i++) {
+    for(let i = 1; i <= 2500; i++) {
         array.push({
             id: i,
             text: `할 일 ${i}`,
